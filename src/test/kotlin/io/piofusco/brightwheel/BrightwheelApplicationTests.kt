6bc809848f775f434d67ed6068dc2d51f6453f1f
gideon <terinopace@gmail.com>
2019-11-12 22:07:03 -0700

    First commit

---
+++ src/test/kotlin/io/piofusco/brightwheel/BrightwheelApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.piofusco.brightwheel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrightwheelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
